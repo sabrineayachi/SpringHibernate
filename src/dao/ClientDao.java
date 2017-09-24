aacb2dbb73995223b8f946b94a83ad60df3e9fa4
sabrineayachi <sabrinee.ayachi@gmail.com>
2017-09-24 19:25:38 +0100

    Fix some indentations

--- src/dao/ClientDao.java
+++ src/dao/ClientDao.java
@@ -7,32 +7,35 @@ import org.springframework.orm.hibernate3.HibernateTemplate;
 import persistance.Client;
 
 public class ClientDao {
-	
-	private HibernateTemplate hibernateTemplate;
-	
-	public void ajouter(Client client)
-	{ getHibernateTemplate().save(client); }
-	
-	public HibernateTemplate getHibernateTemplate() {
-		return hibernateTemplate;
-	}
 
-	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
-		this.hibernateTemplate = hibernateTemplate;
-	}
+    private HibernateTemplate hibernateTemplate;
 
-	@SuppressWarnings("unchecked")
-	public List<Client> findClients()
-	{ return getHibernateTemplate().find("from Client"); }
-	
-	public void supprimer(Client client)
-	{ getHibernateTemplate().delete(client);
-		
-	}
-	public void modifier(Client client)
-	{ getHibernateTemplate().update(client);
-		
-	}
-	
+    public void ajouter(Client client) {
+        getHibernateTemplate().save(client);
+    }
+
+    public HibernateTemplate getHibernateTemplate() {
+        return hibernateTemplate;
+    }
+
+    public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+        this.hibernateTemplate = hibernateTemplate;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Client> findClients() {
+        return getHibernateTemplate().find("from Client");
+    }
+
+    public void supprimer(Client client) {
+        getHibernateTemplate().delete(client);
+
+    }
+
+    public void modifier(Client client) {
+        getHibernateTemplate().update(client);
+
+    }
+
 
 }
